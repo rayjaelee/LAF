1722a9eb84787110906eec36406faf2e2caec129
Raymond Lee <rayjaelee@gmail.com>
2019-03-11 19:41:02 -0700

    initial commit

---
+++ home.js
@@ -0,0 +1,18 @@
+(function() {
+
+    window.addEventListener("load", initialize);
+  
+    function initialize() {
+      // THIS IS THE CODE THAT WILL BE EXECUTED ONCE THE
+      // WEBPAGE LOADS
+    }
+  
+    function exampleFunction1() {
+      /* SOME CODE */
+    }
+  
+    function $(id) {
+      return document.getElementById(id);
+    }
+  
+})();
